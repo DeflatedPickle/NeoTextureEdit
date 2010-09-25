cee341b1226e13f8037216741dd571eeeeb31da7
Holger Dammertz <holger.dammertz@googlemail.com>
2010-09-25 04:36:25 +0200

    more work on a simple curve class

--- src/engine/parameters/Curve.java
+++ src/engine/parameters/Curve.java
@@ -7,11 +7,19 @@ import java.util.Vector;
 import org.lwjgl.util.vector.Vector2f;
 
 /**
- * This is a simple 2d curve implementation.
+ * This is a simple 1d curve implementation.
+ * 
+ * !!TODO: this class should be generalized to provide a parameterized
+ *         curve with an Interpolator object and a general element that
+ *         can be contained
  * @author Holger Dammertz
  *
  */
 public final class Curve {
+	/** Contains all the control points. adding new points has to make
+	 * sure that the points stay correctly ordered. The same holds true
+	 * for changing the t position of the points.
+	 */
 	private final List<Vector2f> controlPoints = new Vector<Vector2f>();
 	
 	public Curve() {
@@ -63,15 +71,6 @@ public final class Curve {
 		controlPoints.clear();
 	}
 	
-	/**
-	 * Uses the current interpolation settings to return the interpolated
-	 * value at the given position
-	 * @return
-	 */
-	public float value() {
-		//!!TODO:
-		return 0.0f;
-	}
 	
 	/**
 	 * Returns the minimum x value of this curve. If the curve does not
@@ -111,4 +110,57 @@ public final class Curve {
 		p.x = x;
 		_addControlPoint(p); 
 	}
+	
+	/**
+	 * Uses the current interpolation settings to interpolate the control
+	 * points at the given position. If the number of control points is 0
+	 * 0.0f is returned
+	 *
+	 * !!TODO: currently it is only a simple linear interpolation; some higher
+	 * order interpolation will be needed.
+	 * 
+	 * @param t
+	 * @return
+	 */
+	public float interpolate(float t) {
+		if (controlPoints.size() == 0) return 0.0f;
+		//!!TODO: optimize
+
+		// we are left from the smallest control point
+		if (t < controlPoints.get(0).x) 
+			return controlPoints.get(0).y;
+		
+		for (int i = 0; i < controlPoints.size()-1; i++) {
+			if (controlPoints.get(i+1).x > t) {
+				float x0 = controlPoints.get(i).x;
+				float x1 = controlPoints.get(i+1).x;
+				float v = (t-x0)/(x1-x0);
+				
+				return (1.0f-v)*controlPoints.get(i).y + (v)*controlPoints.get(i+1).y;
+			}
+		}
+		
+		// we are right from the rightmost control point
+		return controlPoints.get(controlPoints.size()-1).y;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Curve c = new Curve();
+		
+		c.addControlPoint(new Vector2f(0.5f, 0.0f));
+		c.addControlPoint(new Vector2f(1.0f, 1.0f));
+		c.addControlPoint(new Vector2f(0.0f, 1.0f));
+		
+		System.out.println(c.interpolate(-1.0f));
+		System.out.println(c.interpolate(0.0f));
+		System.out.println(c.interpolate(0.1f));
+		System.out.println(c.interpolate(0.5f));
+		System.out.println(c.interpolate(0.75f));
+		
+		
+	}
+	
+	
 }
--- src/engine/parameters/CurveParam.java
+++ src/engine/parameters/CurveParam.java
@@ -1,5 +1,40 @@
 package engine.parameters;
 
-public final class CurveParam  extends AbstractParam {
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Scanner;
 
+public final class CurveParam  extends AbstractParam {
+	private final Curve curve;
+	
+	private CurveParam(String name, int numCP, float min, float max) {
+		this.name = name;
+		curve = new Curve();
+	}
+	
+	public Curve getCurve() {
+		return curve;
+	}
+	
+	public void save(Writer w) throws IOException  {
+		//!!TODO
+		//w.write(valuePos+" ");
+	}
+
+	public void load(Scanner s) {
+		//!!TODO
+		//setEnumPos(s.nextInt());
+	}
+	
+	public static CurveParam Create(String name, int numCP, float min, float max) {
+		CurveParam ret = new CurveParam(name, numCP, min, max);
+		return ret;
+	}
+
+	
+	public static CurveParam CreateManaged(String name, int numCP, float min, float max) {
+		CurveParam ret = Create(name, numCP, min, max);
+		ParameterManager.add(ret);
+		return ret;
+	}
 }
