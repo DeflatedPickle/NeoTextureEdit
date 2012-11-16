d623444e85f470561762ba600c0ce45a577befcb
Holger Dammertz <holger.dammertz@googlemail.com>
2012-11-16 20:36:44 +0100

    added experimental FilterMath1

--- src/com/mystictri/neotextureedit/TextureEditor.java
+++ src/com/mystictri/neotextureedit/TextureEditor.java
@@ -152,7 +152,7 @@ public class TextureEditor implements ActionListener, KeyListener {
 					"FilterMask.class", "FilterNormalMap.class", "FilterWarp.class", "Pattern.class", "PatternBrick.class",
 					"PatternCellular.class", "PatternChecker.class", "PatternConstantColor.class", "PatternGradient.class",
 					"PatternPerlinNoise.class", "PatternTile.class", "PatternFunction.class", "PatternBitmap.class", "FilterIlluminate.class",
-					"FilterCombine.class", "FilterTransform.class", "FilterBlur.class", "FilterModulus.class"};
+					"FilterCombine.class", "FilterTransform.class", "FilterBlur.class", "FilterModulus.class", "FilterMath1.class"};
 			files = f;
 		}
 
